05ca99e18020495ec9605e2189ffeee6e6c2d0fb
Frederico Garcia <fredericomelogarcia@outlook.com>
2016-09-07 19:27:49 +0100

    test

--- git.sh
+++ git.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "$1"
-git push -u origin master
+#!/bin/bash -l
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+  else
+    git add .
+    git commit -m "$1"
+    git push -u origin master
+fi
